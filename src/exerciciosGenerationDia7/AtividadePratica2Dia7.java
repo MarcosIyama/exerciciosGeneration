4bc52edb5c922108af4d0e7e73d2d15727d0c46c
Marcos Iyama <marcosiyama@gmail.com>
2024-12-03 13:09:14 -0300

    Adicionei AtividadePratica2Dia7

---
+++ src/exerciciosGenerationDia7/AtividadePratica2Dia7.java
@@ -0,0 +1,41 @@
+package exerciciosGenerationDia7;
+
+import java.util.Scanner;
+
+public class AtividadePratica2Dia7 {
+
+	public static void main(String[] args) {
+		// Variáveis
+		float numero;
+
+		Scanner leia = new Scanner(System.in);
+
+		// Entrada
+		System.out.println("Digite um número: ");
+		numero = leia.nextFloat();
+
+		// Processamento
+		if (numero > 0) {
+			if ((numero % 2) == 0) {
+				System.out.printf("O Número %.0f é par e positivo!", numero);
+
+			} else if ((numero % 2) == 1) {
+				System.out.printf("O Número %.0f é ímpar e positivo!", numero);
+
+			}
+		} else if (numero < 0) {
+			if ((numero % 2) == 0) {
+				System.out.printf("O Número %.0f é par e negativo!", numero);
+
+			} else if ((numero % 2) == -1) {
+				System.out.printf("O Número %.0f é ímpar e negativo!", numero);
+
+			}
+		} else {
+			System.out.println("Númeral 0 é inválido.");
+		}
+		leia.close();
+
+	}
+
+}
