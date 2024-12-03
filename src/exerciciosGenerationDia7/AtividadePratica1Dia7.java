2d27ce29f1ee355659408a7cfc9bbf23f5c178b0
Marcos Iyama <marcosiyama@gmail.com>
2024-12-03 12:28:01 -0300

    Adicionei AtividadePratica1Dia7

---
+++ src/exerciciosGenerationDia7/AtividadePratica1Dia7.java
@@ -0,0 +1,39 @@
+package exerciciosGenerationDia7;
+
+import java.util.Scanner;
+
+public class AtividadePratica1Dia7 {
+
+	public static void main(String[] args) {
+		// Variáveis
+		float nA;
+		float nB;
+		float nC;
+
+		Scanner leia = new Scanner(System.in);
+
+		// Entrada
+		System.out.println("Digite o número A: ");
+		nA = leia.nextFloat();
+
+		System.out.println("Digite o número B: ");
+		nB = leia.nextFloat();
+
+		System.out.println("Digite o número C: ");
+		nC = leia.nextFloat();
+
+		// Processamento
+		if (nA + nB > nC) {
+			System.out.println("A soma de A + B é MAIOR do que C");
+
+		} else if (nA + nB < nC) {
+			System.out.println("A soma de A + B é MENOR do que C");
+
+		} else {
+			System.out.println("A soma de A + B é IGUAL a C");
+
+		}
+		leia.close();
+
+	}
+}
